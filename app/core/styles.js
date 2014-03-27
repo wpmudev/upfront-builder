5a197f5467ef513a3b51601650ae5718aa3a4368
javier <javier@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-03-27 18:22:58 +0000

    Hacked Upfront CSS editor to let the developer pick images from the theme's image folder.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/upfront-theme-exporter@13478 f53fe46e-8c02-459b-9fdd-93e75771b278

--- app/core/styles.js
+++ app/core/styles.js
@@ -18,6 +18,7 @@ define([
 
       Upfront.Events.on('Upfront:loaded', function(){
         me.fetchDefaultStyles();
+        me.setThemeCSSImages();
       });
     },
 
@@ -62,6 +63,33 @@ define([
         _.each(styles, function(style, elementId) {
           body.append('<style id="upfront-default-style-' + elementId + '">' + style + '</style>');
         });
+      });
+    },
+
+    setThemeCSSImages: function(){
+      var editor = Upfront.Application.cssEditor;
+
+      delete(editor.events['click .upfront-css-image']);
+
+      editor.delegateEvents();
+      editor.$el.on('click', '.upfront-css-image', _.bind(this.openImagePicker, this));
+    },
+
+    openImagePicker: function(){
+      var me = this,
+        editor = Upfront.Application.cssEditor
+      ;
+
+      Upfront.Media.Manager.open({
+        themeImages: true
+      }).done(function(popup, result){
+        Upfront.Events.trigger('upfront:element:edit:stop');
+        if(!result)
+          return;
+
+        var url = result.models[0].get('original_url').replace(document.location.origin, '');
+        editor.editor.insert('url("' + url + '")');
+        editor.editor.focus();
       });
     }
   };
