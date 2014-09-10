bb16266da09f48d18811221cd88d963cdd648b52
vladislav <ve@incsub.com>
2014-09-10 07:58:46 +0200

    Adding the default 404 page layout.

---
+++ templates/default_layouts/single-404_page.php
@@ -0,0 +1,63 @@
+<?php
+$main_nav = upfront_create_region(array(
+	"name" => "main_nav",
+	"title" => "Navigation",
+	"type" => "wide",
+	"scope" => "local"
+), array(
+	"background_type" => "color",
+	"background_color" => "#fff"
+));
+$main_nav->add_element("PlainTxt", array(
+	"columns" => "24",
+	"margin_left" => "0",
+	"margin_right" => "0",
+	"margin_top" => "6",
+	"margin_bottom" => "0",
+	"id" => "default-nav-text-module",
+	"rows" => 12,
+	"options" => array(
+		"view_class" => "PlainTxtView",
+		"id_slug" => "plaintxt",
+		"content" => "<h3>404 page</h3>",
+		"element_id" => "default-nav-text-object",
+		"class" => "c24",
+		"type" => "PlainTxtModel",
+		"has_settings" => 1
+	)
+));
+$regions->add($main_nav);
+
+$main = upfront_create_region(array(
+	'name' => "main",
+	'title' => __("Main Area"),
+	'scope' => "local",
+	'type' => 'wide',
+	'default' => true,
+	'allow_sidebar' => true
+), array(
+	'row' => 140,
+	'background_type' => 'color',
+	'background_color' => '#c5d0db'
+));
+
+$main->add_element("PlainTxt", array(
+	"columns" => "24",
+	"margin_left" => "0",
+	"margin_right" => "0",
+	"margin_top" => "6",
+	"margin_bottom" => "0",
+	"id" => "default-nav-text-module",
+	"rows" => 12,
+	"options" => array(
+		"view_class" => "PlainTxtView",
+		"id_slug" => "plaintxt",
+		"content" => "<p>Uh oh, somehting went wrong!</p>",
+		"element_id" => "default-nav-text-object",
+		"class" => "c24",
+		"type" => "PlainTxtModel",
+		"has_settings" => 1
+	)
+));
+
+$regions->add($main);
