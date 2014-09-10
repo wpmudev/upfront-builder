0b58e70ed4ff6f34f52e6aaaf5fe4a9dccefcc42
vladislav <ve@incsub.com>
2014-09-10 07:19:30 +0200

    Adding in the dedicated admin handler.
    Currently, this only serves admin notices.

---
+++ class_thx_admin.php
@@ -0,0 +1,42 @@
+<?php
+
+class Thx_Admin {
+
+	private function __construct () {}
+	private function __clone () {}
+
+	public static function serve () {
+		$me = new self;
+		$me->_add_hooks();
+	}
+
+	private function _add_hooks () {
+		if (!is_admin()) return false;
+		if (defined('DOING_AJAX') && DOING_AJAX) return false;
+
+		add_action('admin_notices', array($this, 'dispatch_notices'));
+	}
+
+	/**
+	 * The notices dispatch hub method.
+	 * Each of the array values should be a single string which will be processed, wrapped in Ps and rendered.
+	 */
+	public function dispatch_notices () {
+		$notices = array_filter(apply_filters('upfront-thx-admin_notices', array(
+			$this->_permalink_setup_check_notice(),
+		)));
+		if (empty($notices)) return false;
+		echo '<div class="error"><p>' .
+			join('</p><p>', $notices) .
+		'</p></div>';
+	}
+
+	private function _permalink_setup_check_notice () {
+		if (get_option('permalink_structure')) return false;
+		$msg = sprintf(
+			__('Upfront Exporter requires Pretty Permalinks to work. Please enable them <a href="%s">here</a>', 'upfront_thx'),
+			admin_url('/options-permalink.php')
+		);
+		return $msg;
+	}
+}
