5398e7623e1a7308a7a2a733061c65f3f028eda7
vladislav <ve@incsub.com>
2014-09-12 07:24:06 +0200

    Refactoring the slider import method.

--- templates/functions.php
+++ templates/functions.php
@@ -39,7 +39,7 @@ class %name% extends Upfront_ChildTheme {
     }
 
     public function augment_regions ($regions) {
-        if ($this->_slider_imported) return $regions;
+        if (!empty($this->_slider_imported)) return $regions;
 
         if (empty($regions) || !is_array($regions)) return $regions;
         foreach ($regions as $idx => $region) {
@@ -55,37 +55,6 @@ class %name% extends Upfront_ChildTheme {
 
         $this->_slider_imported = true;
         return $regions;
-    }
-
-    private function _import_slider_image ($filepath) {
-        $key = $this->get_prefix() . '-slider-images';
-        $images = get_option($key, array());
-        if (!empty($images[$filepath])) return $images[$filepath];
-        // else import image
-        $wp_upload_dir = wp_upload_dir();
-        $pfx = !empty($wp_upload_dir['path']) ? trailingslashit($wp_upload_dir['path']) : '';
-        if (!function_exists('wp_generate_attachment_metadata')) require_once(ABSPATH . 'wp-admin/includes/image.php');
-        $filename = basename($filepath);
-        while (file_exists("{$pfx}{$filename}")) {
-            $filename = rand() . $filename;
-        }
-        @copy($filepath, "{$pfx}{$filename}");
-        $wp_filetype = wp_check_filetype(basename($filename), null);
-        $attachment = array(
-            'guid' => $wp_upload_dir['url'] . '/' . basename($filename),
-            'post_mime_type' => $wp_filetype['type'],
-            'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
-            'post_content' => '',
-            'post_status' => 'inherit'
-        );
-        $attach_id = wp_insert_attachment($attachment, "{$pfx}{$filename}");
-        $attach_data = wp_generate_attachment_metadata( $attach_id, "{$pfx}{$filename}" );
-        wp_update_attachment_metadata( $attach_id, $attach_data );
-
-        $images[$filepath] = $attach_id;
-        update_option($key, $images);
-
-        return $attach_id;
     }
 }
 
