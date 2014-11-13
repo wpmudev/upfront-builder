1d75812c5f12b0b5b4df3b614250be42678ac82e
Sam Najian <dev.samnajian@gmail.com>
2014-11-13 10:05:58 +0330

    remove default top nav region

--- templates/default_layouts/archive.php
+++ templates/default_layouts/archive.php
@@ -1,34 +1,5 @@
 <?php
 return '<?php
-$main_nav = upfront_create_region(array(
-	"name" => "main_nav",
-	"title" => "Navigation",
-	"type" => "wide",
-	"scope" => "local"
-), array(
-	"background_type" => "color",
-	"background_color" => "#fff"
-));
-$main_nav->add_element("PlainTxt", array(
-	"columns" => "24",
-	"margin_left" => "0",
-	"margin_right" => "0",
-	"margin_top" => "6",
-	"margin_bottom" => "0",
-	"id" => "default-nav-text-module",
-	"rows" => 12,
-	"options" => array(
-		"view_class" => "PlainTxtView",
-		"id_slug" => "plaintxt",
-		"content" => "<h3>Archive</h3>",
-		"element_id" => "default-nav-text-object",
-		"class" => "c24",
-		"type" => "PlainTxtModel",
-		"has_settings" => 1
-	)
-));
-$regions->add($main_nav);
-
 $content = upfront_create_region(array(
 	"name" => "content",
 	"title" => "Content Area",
