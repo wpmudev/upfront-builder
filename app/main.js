8ab7c470af995cf587dc86a08d70a195d5d5ec36
Sam Najian <dev.samnajian@gmail.com>
2014-11-18 11:08:01 +0330

    fix dependency cascase in main.js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 (function() {
 	var dependencies = [
 		Upfront.themeExporter.root + 'app/styles.js',
-		Upfront.themeExporter.root + 'app/postlayout.js',
-        Upfront.themeExporter.root + 'app/post_image.js'
+        Upfront.themeExporter.root + 'app/post_image.js',
+        Upfront.themeExporter.root + 'app/postlayout.js'
     ]
-  require(dependencies, function(StylesHelper, PostLayoutHelper){
+  require(dependencies, function(StylesHelper, postImage, PostLayoutHelper){
     StylesHelper.init();
     PostLayoutHelper.init();
   });
