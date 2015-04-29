5ac3943fa6dd75365cec0220999542b49682b2bd
vladislav <ve@incsub.com>
2015-04-29 09:36:14 +0200

    Start refactoring the JS.

--- app/main.js
+++ app/main.js
@@ -1,10 +1,48 @@
-(function() {
+(function($) {
 	var dependencies = [
 		Upfront.themeExporter.root + 'app/styles.js',
-        Upfront.themeExporter.root + 'app/postlayout.js'
-    ]
-  require(dependencies, function(StylesHelper, PostLayoutHelper){
-    StylesHelper.init();
-    PostLayoutHelper.init();
-  });
-})();
+		Upfront.themeExporter.root + 'app/postlayout.js'
+	]
+	require(dependencies, function(StylesHelper, PostLayoutHelper){
+		StylesHelper.init();
+		PostLayoutHelper.init();
+
+		/*
+		var cView = Backbone.View.extend({
+			events: {
+				'click a[href="#change"]': 'spawn_info_change_interface'
+			},
+			render: function () {
+				this.$el.empty().append('<a href="#change">edit info</a>');
+			},
+			spawn_info_change_interface: function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+
+				this._modal = new Upfront.Views.Editor.Modal({to: $('body'), button: false, top: 120, width: 540});
+				this._modal.render();
+				$('body').append(this._modal.el);
+
+				var me = this;
+				
+				Upfront.Util.post({action: 'upfront_thx-get-theme_info'})
+					.done(function () {
+						me._modal.open(function ($content) {
+							console.log("yay open")
+							$content.append("NANANANAN");
+						});
+					})
+				;
+			}
+		});
+
+		$(document).on("upfront-load", function () {
+			Upfront.Events.on("application:mode:after_switch", function () {
+				Upfront.Application.sidebar.sidebar_commands.additional = new cView({model: Upfront.Application.sidebar.model});
+				Upfront.Application.sidebar.render();
+			});
+		});
+		*/
+
+	});
+})(jQuery);
