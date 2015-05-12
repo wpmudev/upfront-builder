a490561940ea37a19a1f2a2fc112c84375bb987d
vladislav <ve@incsub.com>
2015-05-12 09:23:04 +0200

    Update the forms to proper format.

--- templates/plugin/create_edit.php
+++ templates/plugin/create_edit.php
@@ -8,18 +8,23 @@
 	}
 ?>
 
-<div class="uf-thx-initial">
+<header class="builder">
+	<h2><?php esc_html_e('Upfront Builder', UpfrontThemeExporter::DOMAIN); ?></h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</header>
+
+<div class="uf-thx-initial clearfix">
 	
 	<div class="uf-thx-existing_theme uf-thx-pane">
 	<?php if (!empty($themes)) { ?>
-		<h3><?php esc_html_e('Modify existing theme', UpfrontThemeExporter::DOMAIN); ?></h3>
+		<h3><?php esc_html_e('Modify existing theme:', UpfrontThemeExporter::DOMAIN); ?></h3>
 		
 		<div class="uf-thx-themes_container clearfix">
 		<?php foreach ($themes as $key => $theme) { ?>
 			<div class="uf-thx-theme <?php if (!empty($_GET['theme']) && $theme->get_stylesheet() === $_GET['theme']) echo 'current'; ?>">
 				<a href="?theme=<?php echo esc_attr($theme->get_stylesheet()); ?>">
 					<?php $screenshot = $theme->get_screenshot() ? $theme->get_screenshot() : $fallback_screenshot; ?>
-					<img src="<?php echo esc_url($screenshot); ?>" width="50" height="50" />
+					<img src="<?php echo esc_url($screenshot); ?>" />
 					<div class="uf-thx-caption">
 						<b><?php echo esc_html($theme->get('Name')); ?></b>
 					</div>
@@ -43,11 +48,11 @@
 
 	<div class="uf-thx-new_theme uf-thx-pane">
 	<?php if (empty($_GET['theme'])) { ?>
-		<h3><?php esc_html_e('Build a new theme', UpfrontThemeExporter::DOMAIN); ?></h3>
+		<h3><?php esc_html_e('Build a new theme:', UpfrontThemeExporter::DOMAIN); ?></h3>
 
 		<?php Thx_Template::plugin()->load('theme_form'); ?>
 
-		<button type="button" class="create info">
+		<button type="button" class="create theme">
 			<?php esc_html_e('Start building', UpfrontThemeExporter::DOMAIN); ?>
 		</button>
 	<?php } else { ?>
@@ -67,6 +72,7 @@
 				'licence_uri' => $theme->get('LicenceURI'),
 				'tags' => $theme->get('Tags'),
 				'text_domain' => $theme->get('TextDomain'),
+				'screenshot' => ($theme->get_screenshot() ? $theme->get_screenshot() : $fallback_screenshot)
 			)); 
 		?>
 
@@ -107,20 +113,27 @@ function get_data () {
 
 function init_new () {
 	$(".uf-thx-new_theme")
-		.on("click", "button.create.info", function (e) {
+		.on("click", "button.create.theme", function (e) {
 			e.preventDefault();
 			e.stopPropagation();
 
-			var data = get_data();
-			if (!data['thx-theme-slug']) return false;
-			
+			var data = get_data(),
+				slug
+			;
+			if ('thx-theme-slug' in data) {
+				slug = data['thx-theme-slug'];
+			}
 			data.add_global_regions = true;
 
 			Upfront.Util.post({
 				action: 'upfront_thx-create-theme',
-				form: _.map(data, function(value, key){ return key + '=' + value; }).join('&')
-			}).success(function(response){
-				window.location.pathname = window.location.pathname.replace(/\/theme/, '/' + data['thx-theme-slug']);
+				form: _.map(data, function(value, key){ return key + '=' + escape(value); }).join('&')
+			}).success(function(response) {
+				if (!slug && response && "theme" in response) {
+					slug = (response.theme || {directory: false}).directory;
+				}
+				if (slug) window.location.pathname = window.location.pathname.replace(/\/theme/, '/' + slug);
+				else window.location.reload();
 			}).error(function(){
 				console.log("error");
 			});
@@ -138,8 +151,8 @@ function init_new () {
 
 			Upfront.Util.post({
 				action: 'upfront_thx-update-theme',
-				form: _.map(data, function(value, key){ return key + '=' + value; }).join('&')
-			}).success(function(response){
+				form: _.map(data, function(value, key){ return key + '=' + escape(value); }).join('&')
+			}).success(function(response) {
 				window.location.reload();
 			}).error(function(){
 				console.log("error");
@@ -163,6 +176,29 @@ function init_new () {
 
 function init_existing () {
 	$(".uf-thx-existing_theme")
+		.on("click", ".uf-thx-theme a", function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+
+			var $me = $(this).closest(".uf-thx-theme");
+			if (!$me.length) return false;
+
+			$(".uf-thx-existing_theme .uf-thx-theme").removeClass("current");
+			$me.addClass("current");
+
+			return false;
+		})
+		.on("click", "button.edit.info", function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+			
+			var current = $(".uf-thx-existing_theme .uf-thx-theme.current a").attr('href');
+			if (!current) return false;
+			
+			window.location = current;
+
+			return false;			
+		})
 		.on("click", "button.edit.theme", function (e) {
 			e.preventDefault();
 			e.stopPropagation();
@@ -188,34 +224,136 @@ $(document).on("upfront-load", init);
 </script>
 
 <style type="text/css">
+body {
+	background: rgb(241, 241, 241);
+	color: rgb(76, 83, 85);
+}
+header.builder {
+	border-bottom: 1px solid rgb(216, 216, 216);
+	padding: 40px;
+	text-align: center;
+}
 .uf-thx-initial {
-	width: 80%;
 	min-width: 400px;
-	clear: both;
 }
 .uf-thx-pane {
 	float: left;
-	width: 49%;
+	width: 48%;
 	padding: .5%;
-}
-.uf-thx-pane label {
-	display: block;
 }
 
 .uf-thx-new_theme {
 	left: 0;
+	margin-left: -1px;
+	border-left: 1px solid rgb(216, 216, 216);
+	padding-left: 20px;
 }
-
 .uf-thx-existing_theme {
 	left: 50%;
+	border-right: 1px solid rgb(216, 216, 216);
 }
+.uf-thx-existing_theme h3 {
+	padding-left: 20px;
+}
+
+/* forms */
+.uf-thx-pane h3 {
+	margin-bottom: 1em;
+}
+.uf-thx-pane label {
+	display: block;
+	color: rgb(109, 114, 116);
+	margin-bottom: 1em;
+}
+.uf-thx-pane label.inline {
+	margin-bottom: 0;
+}
+.uf-thx-pane label span.description {
+	display: block;
+}
+.uf-thx-pane label.inline span.description {
+	display: inline;
+}
+.uf-thx-pane label input[type="text"],
+.uf-thx-pane label textarea
+{
+	border: 5px solid rgb(223, 223, 223);
+	padding: 15px;
+	min-width: 300px;
+	width: 100%;
+	color: rgb(109, 114, 116);
+	font-size: 14px;
+}
+button {
+	color: rgb(126, 131, 132);
+	background: none;
+}
+button.theme {
+	padding: 12px 46px;
+	border: 5px solid rgb(31, 205, 143);
+	font-weight: bold;
+}
+button.info {
+	border-bottom: 1px solid rgb(31, 205, 143);
+}
+
+/* list */
 .uf-thx-existing_theme .uf-thx-theme {
-	width: 50px;
-	overflow: hidden;
+	width: 110px;
 	float: left;
-	margin-left: 10px;
+	margin: 15px;
+	position: relative;
+	border: 3px solid transparent;
 }
+.uf-thx-existing_theme .uf-thx-theme img {
+	width: 110px;
+	min-height: 110px;
+}
+.uf-thx-existing_theme .uf-thx-theme .uf-thx-caption {
+	width: 100%;
+	overflow: hidden;
+	background: rgba(100, 100, 100, .7);
+	position: absolute;
+	bottom: 3px;
+	display: none;
+}
+.uf-thx-existing_theme .uf-thx-theme:hover .uf-thx-caption {
+	display: block;
+}
+.uf-thx-existing_theme .uf-thx-theme .uf-thx-caption b {
+	margin: 9px 18px;
+	display: block;
+	font-weight: normal;
+	color: #fff;
+}
+/* list - current */
 .uf-thx-existing_theme .uf-thx-theme.current {
-	border: 5px solid red;
+	border: 3px solid rgb(31, 205, 143);
+}
+.uf-thx-existing_theme .uf-thx-theme.current .uf-thx-caption {
+	bottom: 0;
+}
+.uf-thx-existing_theme .uf-thx-theme.current:before {
+	content: "\2714";
+	display: block;
+	position: absolute;
+	right: -5px;
+	top: -5px;
+	width: 30px;
+	height: 30px;
+	background: rgb(35, 49, 64);
+	color: rgb(31, 205, 143);
+	text-align: center;
+	line-height: 30px;
+}
+
+/* info pane */
+.uf-thx-theme_info .uf-thx-theme_screenshot {
+	float: left;
+	width: 370px;
+}
+.uf-thx-theme_info .uf-thx-theme_meta {
+	float: left;
+	margin-left: 20px;
 }
 </style>
--- templates/plugin/theme_form.php
+++ templates/plugin/theme_form.php
@@ -1,68 +1,66 @@
-<label for="theme-name">
-	<?php esc_html_e('Name', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="theme-name" placeholder="<?php esc_attr_e('Name for your theme', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($name)) echo esc_attr($name); ?>" />
-</label>
-<label for="theme-slug">
-	<?php esc_html_e('Directory', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="theme-slug" placeholder="<?php esc_attr_e('Base directory for your theme', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($slug)) echo esc_attr($slug); ?>" />
-</label>
-<label for="author">
-	<?php esc_html_e('Author', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="author" placeholder="<?php esc_attr_e('Your name', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($author)) echo esc_attr($author); ?>" />
-</label>
-<label for="author-uri">
-	<?php esc_html_e('Author URL', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="author-uri" placeholder="<?php esc_attr_e('Your URL', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($author_uri)) echo esc_attr($author_uri); ?>" />
-</label>
+<div class="uf-thx-theme_info clearfix">
+	<div class="uf-thx-theme_screenshot">
+		<label>
+			<span class="description"><?php esc_html_e('Theme listing preview:', UpfrontThemeExporter::DOMAIN); ?></span>
+			<img src="<?php if (!empty($screenshot)) echo esc_url($screenshot); ?>" width="370" height="255" />
+		</label>
+	</div>
+	<div class="uf-thx-theme_meta">
+		<label for="theme-name">
+			<span class="description"><?php esc_html_e('Name', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="theme-name" placeholder="<?php esc_attr_e('Name for your theme', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($name)) echo esc_attr($name); ?>" />
+		</label>
+		<label for="author">
+			<span class="description"><?php esc_html_e('Author', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="author" placeholder="<?php esc_attr_e('Your name', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($author)) echo esc_attr($author); ?>" />
+		</label>
+		<label for="author-uri">
+			<span class="description"><?php esc_html_e('Author URL', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="author-uri" placeholder="<?php esc_attr_e('Your URL', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($author_uri)) echo esc_attr($author_uri); ?>" />
+		</label>
 
-<?php if (!empty($description)) { ?>
-<label for="description">
-	<?php esc_html_e('Description', UpfrontThemeExporter::DOMAIN); ?>
-	<textarea id="description"><?php if (!empty($description)) echo esc_textarea($description); ?></textarea>
-</label>
-<?php } ?>
-<?php if (!empty($version)) { ?>
-<label for="version">
-	<?php esc_html_e('Version', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="version" value="<?php if (!empty($version)) echo esc_attr($version); ?>" />
-</label>
-<?php } ?>
-<?php if (!empty($theme_uri)) { ?>
-<label for="theme-uri">
-	<?php esc_html_e('Theme URI', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="theme-uri" value="<?php if (!empty($theme_uri)) echo esc_attr($theme_uri); ?>" />
-</label>
-<?php } ?>
-<?php if (!empty($licence)) { ?>
-<label for="licence">
-	<?php esc_html_e('Licence', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="licence" value="<?php if (!empty($licence)) echo esc_attr($licence); ?>" />
-</label>
-<?php } ?>
-<?php if (!empty($licence_uri)) { ?>
-<label for="licence-uri">
-	<?php esc_html_e('Licence URI', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="licence-uri" value="<?php if (!empty($licence_uri)) echo esc_attr($licence_uri); ?>" />
-</label>
-<?php } ?>
-<?php if (!empty($tags)) { ?>
-<label for="tags">
-	<?php esc_html_e('Tags', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="tags" value="<?php if (!empty($tags)) echo esc_attr(join(', ', $tags)); ?>" />
-</label>
-<?php } ?>
-<?php if (!empty($text_domain)) { ?>
-<label for="text-domain">
-	<?php esc_html_e('Text Domain', UpfrontThemeExporter::DOMAIN); ?>
-	<input type="text" id="text-domain" value="<?php if (!empty($text_domain)) echo esc_attr($text_domain); ?>" />
-</label>
-<?php } ?>
+	<?php if (!empty($name)) { /* if we're not creating a new theme, show all */ ?>
+		<label for="theme-slug">
+			<span class="description"><?php esc_html_e('Directory', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="theme-slug" placeholder="<?php esc_attr_e('Base directory for your theme', UpfrontThemeExporter::DOMAIN); ?>" value="<?php if (!empty($slug)) echo esc_attr($slug); ?>" />
+		</label>
+		<label for="description">
+			<span class="description"><?php esc_html_e('Description', UpfrontThemeExporter::DOMAIN); ?></span>
+			<textarea id="description"><?php if (!empty($description)) echo esc_textarea($description); ?></textarea>
+		</label>
+		<label for="version">
+			<span class="description"><?php esc_html_e('Version', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="version" value="<?php if (!empty($version)) echo esc_attr($version); ?>" />
+		</label>
+		<label for="theme-uri">
+			<span class="description"><?php esc_html_e('Theme URI', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="theme-uri" value="<?php if (!empty($theme_uri)) echo esc_attr($theme_uri); ?>" />
+		</label>
+		<label for="licence">
+			<span class="description"><?php esc_html_e('Licence', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="licence" value="<?php if (!empty($licence)) echo esc_attr($licence); ?>" />
+		</label>
+		<label for="licence-uri">
+			<span class="description"><?php esc_html_e('Licence URI', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="licence-uri" value="<?php if (!empty($licence_uri)) echo esc_attr($licence_uri); ?>" />
+		</label>
+		<label for="tags">
+			<span class="description"><?php esc_html_e('Tags', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="tags" value="<?php if (!empty($tags)) echo esc_attr(join(', ', $tags)); ?>" />
+		</label>
+		<label for="text-domain">
+			<span class="description"><?php esc_html_e('Text Domain', UpfrontThemeExporter::DOMAIN); ?></span>
+			<input type="text" id="text-domain" value="<?php if (!empty($text_domain)) echo esc_attr($text_domain); ?>" />
+		</label>
+	<?php } ?>
 
-<label for="activate_theme">
-	<input type="checkbox" checked id="activate_theme" />
-	<?php esc_html_e('Activate theme', UpfrontThemeExporter::DOMAIN); ?>
-</label>
-<label for="export_with_images">
-	<input type="checkbox" checked id="export_with_images" />
-	<?php esc_html_e('Export with images', UpfrontThemeExporter::DOMAIN); ?>
-</label>
+		<label for="activate_theme" class="inline">
+			<input type="checkbox" checked id="activate_theme" />
+			<span class="description"><?php esc_html_e('Activate theme', UpfrontThemeExporter::DOMAIN); ?></span>
+		</label>
+		<label for="export_with_images" class="inline">
+			<input type="checkbox" checked id="export_with_images" />
+			<span class="description"><?php esc_html_e('Export with images', UpfrontThemeExporter::DOMAIN); ?></span>
+		</label>
+	</div>
+</div>
