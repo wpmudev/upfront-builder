0322db3b45fa0221324f884c19b782fc147541bc
vladislav <ve@incsub.com>
2015-07-29 10:16:34 +0200

    Load up the ported stuff.
    Temporarily disabled, so people can carry on working.

--- app/main.js
+++ app/main.js
@@ -7,46 +7,16 @@
 		StylesHelper.init();
 		PostLayoutHelper.init();
 
-		/*
-		var cView = Backbone.View.extend({
-			events: {
-				'click a[href="#change"]': 'spawn_info_change_interface'
-			},
-			render: function () {
-				this.$el.empty().append('<a href="#change">edit info</a>');
-			},
-			spawn_info_change_interface: function (e) {
-				e.preventDefault();
-				e.stopPropagation();
-
-				this._modal = new Upfront.Views.Editor.Modal({to: $('body'), button: false, top: 120, width: 540});
-				this._modal.render();
-				$('body').append(this._modal.el);
-
-				var me = this;
-				
-				Upfront.Util.post({action: 'upfront_thx-get-theme_info'})
-					.done(function () {
-						me._modal.open(function ($content) {
-							console.log("yay open")
-							$content.append("NANANANAN");
-						});
-					})
-				;
-			}
-		});
-
 		$(document).on("upfront-load", function () {
-			Upfront.Events.on("application:mode:after_switch", function () {
-				Upfront.Application.sidebar.sidebar_commands.additional = new cView({model: Upfront.Application.sidebar.model});
-				Upfront.Application.sidebar.render();
-			});
-		});
-		*/
-		$(document).on("upfront-load", function () {
+
 			Upfront.Events.on("upfront:layout:loaded", function () {
 				Upfront.data.global_regions = false; // Reset global regions info on layout load, so fresh batch is forced
 			});
+/*
+			require([Upfront.themeExporter.root  + 'app/commands.js'], function (sidebar) {
+				sidebar.init();
+			});
+*/
 		});
 
 	});
