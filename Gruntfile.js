65f843a072240220a56c668fde5bddbf4f16d2a0
vladislav <ve@incsub.com>
2015-07-30 10:26:56 +0200

    Add `grunt-sass` to exporter toolchain and make the `sass` task default.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 /*global module, require */
 module.exports = function(grunt) {
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-wp-i18n');
 
 	grunt.initConfig({
 		makepot: {
@@ -10,8 +11,20 @@ module.exports = function(grunt) {
 					type: 'wp-plugin'
 				}
 			}
+		},
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'nested',
+				sourceComments: false
+			},
+			dist: {
+				files: {
+					'styles/exporter.css': 'styles/sass/exporter.scss'
+				}
+			}
 		}
 	});
 
-	grunt.registerTask('default', ['makepot']);
+	grunt.registerTask('default', ['sass']);
 };
