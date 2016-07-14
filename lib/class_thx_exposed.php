a3b4dc80ff5ae38e2f3feec43f04d55f99be9157
Diobeth <diobethrayvilla@gmail.com>
2016-07-14 10:31:19 +0800

    added extra checking for deleting user options

--- lib/class_thx_exposed.php
+++ lib/class_thx_exposed.php
@@ -19,12 +19,18 @@ class Thx_Exposed {
 		add_action('upfront_cleanup_user_options', array($this, 'delete_user_options'));
 	}
 	
+	/*
+	* Removing user site option for Builder Getting Started exp
+	*/
 	public function delete_user_options ($store_key) {
-		// removing user site option for Builder Getting Started exp
-		$store_key = (preg_match('/uf-/', $store_key))
-			? $store_key
-			: 'uf-' . $store_key
-		;
-		delete_user_option( get_current_user_id(), $store_key . self::BOOTSTRAP_EXP_SLUG, true );
+		// check first if option existing
+		$store_key = $store_key . self::BOOTSTRAP_EXP_SLUG;
+		$has_option = get_user_option($store_key);
+		if ( !$has_option ) {
+			// if not found, append "uf-"
+			$store_key = 'uf-' . $store_key;
+			$has_option = get_user_option($store_key);
+		}
+		if( $has_option ) delete_user_option( get_current_user_id(), $store_key, true );
 	}
 }
