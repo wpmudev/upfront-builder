4775d3549212560b8bbcf90b68a0b3f0988e52dd
vladislav <ve@incsub.com>
2016-08-24 15:26:31 +0200

    Update the beta version number

--- upfront-theme-exporter.php
+++ upfront-theme-exporter.php
@@ -3,7 +3,7 @@
 Plugin Name: Upfront Builder
 Plugin URI: http://premium.wpmudev.com/
 Description: Exports upfront page layouts to theme.
-Version: 1.0-BETA-2
+Version: 1.0-BETA-3
 Author: WPMU DEV
 Text Domain: upfront_thx
 Author URI: http://premium.wpmudev.com
