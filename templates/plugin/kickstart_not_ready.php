2b9e4a7ef776b01bcdb5fe8a0e42e6a89197d97e
vladislav <ve@incsub.com>
2016-08-26 07:23:45 +0200

    Add themes getting link to no upfront present message.

--- templates/plugin/kickstart_not_ready.php
+++ templates/plugin/kickstart_not_ready.php
@@ -2,5 +2,16 @@
 	<p>
 		<?php esc_html_e('Unfortunately, we do not seem to have Upfront core installed on this site.', UpfrontThemeExporter::DOMAIN); ?>
 		<?php esc_html_e('We need that for the Upfront Builder plugin to work.', UpfrontThemeExporter::DOMAIN); ?>
+		<?php echo wp_kses(
+			sprintf(
+				__('<a href="%s" target="_blank">Get it here.</a>', UpfrontThemeExporter::DOMAIN),
+				'https://premium.wpmudev.org/projects/category/themes/'
+			), array(
+				'a' => array(
+					'href' => array(),
+					'target' => array(),
+				),
+			)
+		); ?>
 	</p>
 </div>
