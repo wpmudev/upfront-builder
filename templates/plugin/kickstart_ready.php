085f66b469c3eb7e7b30ca690153eea6481c6ed0
vladislav <ve@incsub.com>
2017-04-21 05:52:31 +0200

    Augment notice template file

--- templates/plugin/kickstart_ready.php
+++ templates/plugin/kickstart_ready.php
@@ -1,4 +1,4 @@
-<div class="notice notice-info is-dismissible">
+<div class="notice notice-info is-dismissible uf-thx-kickstart">
 	<p>
 		<?php esc_html_e('You do not seem have Upfront theme active on your site, which is needed to make use of the Upfront Builder plugin.', UpfrontThemeExporter::DOMAIN); ?>
 	</p>
@@ -6,6 +6,9 @@
 		<?php esc_html_e('We can fix that for you:', UpfrontThemeExporter::DOMAIN); ?>
 		<button type="button" class="button button-primary" id="upfront-kickstart-start_building">
 			<?php esc_html_e('Start Building', UpfrontThemeExporter::DOMAIN); ?>
+		</button>
+		<button type="button" class="button" id="upfront-kickstart-go_away">
+			<?php esc_html_e('Do not show this again', UpfrontThemeExporter::DOMAIN); ?>
 		</button>
 	</p>
 	<p class="upfront-kickstart-out" style="display:none"></p>
